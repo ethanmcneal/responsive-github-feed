4974da0f6c41aa6a3e8170b8c6060ec4be0a1221
ethan.mcneal <ethan.mcneal@gmail.com>
2021-10-18 17:40:05 -0600

    correct imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./stylesheets/App.css";
+import "./StyleSheets/App.css";
 import { BiUserCircle, BiMenu } from "react-icons/bi";
 import { BsBell, BsGithub } from "react-icons/bs";
 
